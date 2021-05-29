68f93f5519d976f31dbb62f310a910159a3fbadf
monories <85004248+monories@users.noreply.github.com>
2021-05-29 16:09:15 +0900

    git aos
    test

---
+++ AOS.js
@@ -0,0 +1,27 @@
+window.addEventListener('load',function(evnet){
+    AOS.init({
+        // Global settings:
+        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)거짓 ,  // 비활성화 자동 돌연변이 '탐지가 (고급) 
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced) 디 바운스에 지연 창 크기를 조절하면서 사용 (고급 ) 
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+        
+      
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 120, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 400, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+      
+      });
+})
+
+// You can also pass an optional settings object
+// below listed default settings
